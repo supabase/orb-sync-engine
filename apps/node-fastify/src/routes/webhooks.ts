b479e67eb2776ab726ffdc532f0282684ff3cc5e
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2025-05-14 17:52:59 +0800

    fix: increase webhook body limit (#56)
    We're seeing some webhooks up to 4MB.

--- apps/node-fastify/src/routes/webhooks.ts
+++ apps/node-fastify/src/routes/webhooks.ts
@@ -2,7 +2,7 @@ import type { FastifyInstance } from 'fastify';
 
 export default async function routes(fastify: FastifyInstance) {
   fastify.post('/webhooks', {
-    bodyLimit: 2097152,
+    bodyLimit: 5e6, // 5 MB
     handler: async (request, reply) => {
       const headers = request.headers;
       const body: { raw: Buffer } = request.body as { raw: Buffer };
